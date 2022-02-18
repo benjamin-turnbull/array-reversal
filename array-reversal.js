5268e9de4bc90effd83335c676b773f32834438b
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-02-18 15:34:34 +1030

    recreate program functionality with alternative method

--- array-reversal.js
+++ array-reversal.js
@@ -9,10 +9,15 @@ function reverseArray(array) {
 
 // reverses array by modifying the original array by reversing the order of the elements
 function reverseArrayInPlace(array) {
-
+	for (i = 0; i < Math.floor(array.length / 2); i++) {
+		let old = array[i];
+		array[i] = array[array.length - 1 - i];
+		array[array.length - 1 - i] = old;
+	}
+	return array;
 }
 
 numbers = [5, 9, 2, 4, 7];
 
 console.log(reverseArray(numbers));
-// console.log(reverseArrayInPlace(numbers));
+console.log(reverseArrayInPlace(numbers));
